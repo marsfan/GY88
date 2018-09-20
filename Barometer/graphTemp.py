2913835f4b1e7d3149bababee767aae2bcefb632
Gabe R <Lockheedmaniac@gmail.com>
2018-09-19 17:10:35 -0700

    Merge branch 'master' of https://github.com/Marsfan/GY88

--- Barometer/graphTemp.py
+++ Barometer/graphTemp.py
@@ -1,19 +1,27 @@
 import sys
 import time
 import barometer
+import datetime
+import csv
 import matplotlib.pyplot as plt
 
 temparray = []
 x = 0
 
+
 try:
     while True:
+        f = open("data.csv", 'a')
+        writer = csv.writer(f)
+        dattime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         temp = barometer.tempf()
         print(temp)
         print(x)
         x = x + 1
+        writer.writerow((dattime, temp))
         temparray.append(temp)
-        time.sleep(1)
+        f.close()
+        time.sleep(60)
 
 
 except KeyboardInterrupt:
